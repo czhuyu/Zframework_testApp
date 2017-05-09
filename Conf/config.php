544f67a8045668309a062e5fd6e270da753c8b30
czhuyu <czhuyu@gmail.com>
2017-05-09 09:26:41 +0800

    test

---
+++ Conf/config.php
@@ -0,0 +1,17 @@
+<?php  
+
+return [
+	'dbtype'=>'mysql',
+	'host'=>'localhost',
+	'port'=>'3306',
+	'user'=>'root',
+	'passwd'=>'123456',
+	'db'=>'blog',
+	'charset'=>'utf8',
+]
+
+
+
+
+
+?>
--- Controller/IndexController.class.php
+++ Controller/IndexController.class.php
@@ -5,8 +5,26 @@
 namespace App\Controller;
 class IndexController extends \Z\Base\Controller{
 	public function index(){
+
+
+		//$db = new \Z\Base\Db();
+		//echo $db->insert('insert into art (title,cat_id)values(?,?)',['tesertests',5]);
+		//exit;
+		//print_r($db->getAll("select * from php",[]));
+		$db = new \Z\Base\Db();
+		//print_r($db->getOne('select * from art where art_id=?',[57]));
+		$artmodel = new \App\Model\ArtModel();
+		//$artmodel->resolveTable();
+			
+		//$artmodel->find(57);
+		//echo $artmodel->add(['title'=>'frwerqrqwe','cat_id'=>5]);
+
 		$this->assign('title','爽');
 		$this->display('index');
+
+		//throw new \Exception("Error Processing Request", 1);
+		
+
 	}
 }
 
